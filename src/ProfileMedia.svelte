b409fe3557658361ec06edc5118764720d3feb60
Jon Lehman <info@jonlehman.me>
2021-04-06 13:05:59 -0500

    Moving large media to firebase

--- src/ProfileMedia.svelte
+++ src/ProfileMedia.svelte
@@ -24,9 +24,9 @@
 </style>
 
 <!-- svelte-ignore a11y-media-has-caption -->
-<video bind:this={video} poster="assets/JonLehman_Profile_Poster.jpg" autoplay="true" controls="false" preload='metadata' muted loop playsinline>
-    <source src="assets/JonLehman_Profile_SHORT.webm" type="video/webm"/>
-    <source src="assets/JonLehman_Profile_SHORT.mp4" type="video/mp4"/>
+<video bind:this={video} poster="https://firebasestorage.googleapis.com/v0/b/jonlehman-me.appspot.com/o/JonLehman_Profile_Poster.jpg?alt=media&token=9772bb4d-d556-40b8-9ad9-9144ecccb940" autoplay="true" controls="false" preload='metadata' muted loop playsinline>
+    <source src="https://firebasestorage.googleapis.com/v0/b/jonlehman-me.appspot.com/o/JonLehman_Profile_SHORT.webm?alt=media&token=91776b1f-fdcd-4ade-ae2e-f02ad16c717d" type="video/webm"/>
+    <source src="https://firebasestorage.googleapis.com/v0/b/jonlehman-me.appspot.com/o/JonLehman_Profile_SHORT.mp4?alt=media&token=83658efd-3d83-45a4-a749-904311838e48" type="video/mp4"/>
     <!-- svelte-ignore a11y-missing-attribute -->
-    <img src="assets/JonLehman_Profile_Fallback.jpg"/>
+    <img src="https://firebasestorage.googleapis.com/v0/b/jonlehman-me.appspot.com/o/JonLehman_Profile_Fallback.jpg?alt=media&token=3aff2d02-9c05-484f-94a7-a8dc4d8f5c09"/>
 </video>
